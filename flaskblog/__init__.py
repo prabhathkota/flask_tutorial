be5919f8e0ebe1f69602efea961c6217ea0552bf
Prabhath Kota <prabhath.kota@rackspace.com>
2020-01-16 10:53:59 +0530

    flask tutorial second commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+# import secrets
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = 'e41f8a9e82a88b569dcca671d8380ce5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
